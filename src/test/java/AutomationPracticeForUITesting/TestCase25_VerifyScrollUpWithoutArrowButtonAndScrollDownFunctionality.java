3e536f6a652f0025b7e84037339500764bf6af41
Fethiye <fethiye0623@gmail.com>
2023-07-09 23:31:43 +0200

    test case26 added..

--- src/test/java/AutomationPracticeForUITesting/TestCase25_VerifyScrollUpWithoutArrowButtonAndScrollDownFunctionality.java
+++ src/test/java/AutomationPracticeForUITesting/TestCase25_VerifyScrollUpWithoutArrowButtonAndScrollDownFunctionality.java
@@ -1,14 +1,15 @@
 package AutomationPracticeForUITesting;
 
-public class TeTestCase25_VerifyScrollUpUsingArrowButtonAndScrollDownFunctionality {
+public class TestCase25_VerifyScrollUpWithoutArrowButtonAndScrollDownFunctionality {
+
 
     /*
- 1. Launch browser
+    1. Launch browser
 2. Navigate to url 'http://automationexercise.com'
 3. Verify that home page is visible successfully
 4. Scroll down page to bottom
 5. Verify 'SUBSCRIPTION' is visible
-6. Click on arrow at bottom right side to move upward
+6. Scroll up page to top
 7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
      */
 }
