be8d0140cd74c3aef5ef036aa39954f222e07b70
Fethiye <fethiye0623@gmail.com>
2023-07-11 00:50:31 +0200

    tc1 done..

--- src/test/java/AutomationPracticeForUITesting/TestCase1_RegisterUser.java
+++ src/test/java/AutomationPracticeForUITesting/TestCase1_RegisterUser.java
@@ -34,12 +34,8 @@ public class TestCase1_RegisterUser extends ParametersBrowserClass {
      */
 
 
-
-
-
-
     @Test
-    public void test(){
+    public void test() {
 //        1. Launch browser (in der ParametersBrowserClass gemacht ✅)
 
 //        2. Navigate to url 'http://automationexercise.com'
@@ -48,7 +44,7 @@ public class TestCase1_RegisterUser extends ParametersBrowserClass {
 
 //        3. Verify that home page is visible successfully
 
-       Assert.assertTrue(driver.findElement(By.xpath("//a[.=' Home']")).isDisplayed());//passed
+        Assert.assertTrue(driver.findElement(By.xpath("//a[.=' Home']")).isDisplayed());//passed
 
 //        4. Click on 'Signup / Login' button
 
@@ -60,10 +56,11 @@ public class TestCase1_RegisterUser extends ParametersBrowserClass {
 
 //        6. Enter name and email address
 
-        Faker faker_data=new Faker();
+        Faker faker_data = new Faker();
 
-        String name=faker_data.name().firstName();
-        String eMail=faker_data.internet().emailAddress();
+        String name = faker_data.name().firstName();
+        String lastName = faker_data.name().lastName();
+        String eMail = faker_data.internet().emailAddress();
 
         driver.findElement(By.xpath("//input[@name='name']")).sendKeys(name);
         driver.findElement(By.xpath("(//input[@name='email'])[2]")).sendKeys(eMail);
@@ -82,27 +79,69 @@ public class TestCase1_RegisterUser extends ParametersBrowserClass {
 
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("password");
 
-        Select selectDay=new Select(driver.findElement(By.xpath("//select[@id='days']")));
+        Select selectDay = new Select(driver.findElement(By.xpath("//select[@id='days']")));
         selectDay.selectByValue("24");
 
-        Select selectMonth=new Select(driver.findElement(By.xpath("//select[@id='months']")));
+        Select selectMonth = new Select(driver.findElement(By.xpath("//select[@id='months']")));
         selectMonth.selectByValue("4");
 
-        Select selectYear=new Select(driver.findElement(By.xpath("//select[@id='years']")));
+        Select selectYear = new Select(driver.findElement(By.xpath("//select[@id='years']")));
 
         selectYear.selectByValue("1990");
 
 //        10. Select checkbox 'Sign up for our newsletter!'
 
-
+        WebElement signUpCheckBox = driver.findElement(By.xpath("//input[@id='newsletter']"));
+        if (!signUpCheckBox.isSelected()) {
+            signUpCheckBox.click();
+        }
 
 //        11. Select checkbox 'Receive special offers from our partners!'
+
+        WebElement specialOffersCheckBox = driver.findElement(By.xpath("//input[@id='optin']"));
+        if (!specialOffersCheckBox.isSelected()) {
+            specialOffersCheckBox.click();
+        }
+
 //        12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+
+        driver.findElement(By.xpath("//input[@id='first_name']")).sendKeys(name);
+        driver.findElement(By.xpath("//input[@id='last_name']")).sendKeys(lastName);
+        driver.findElement(By.xpath("//input[@id='company']")).sendKeys("Bayer Ag");
+        driver.findElement(By.xpath("//input[@id='address1']")).sendKeys("Bahnallee 12");
+        driver.findElement(By.xpath("//input[@id='address2']")).sendKeys("Bahnallee 14");
+
+        Select selectCountry = new Select(driver.findElement(By.xpath("//select[@id='country']")));
+        selectCountry.selectByValue("Canada");
+        driver.findElement(By.xpath("//input[@id='state']")).sendKeys("married");
+        driver.findElement(By.xpath("//input[@id='city']")).sendKeys("koln");
+        driver.findElement(By.xpath("//input[@id='zipcode']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@id='mobile_number']")).sendKeys("09038283");
+
+
 //        13. Click 'Create Account button'
+        driver.findElement(By.xpath("//button[.='Create Account']")).click();
+
 //        14. Verify that 'ACCOUNT CREATED!' is visible
+
+        Assert.assertTrue(driver.findElement(By.xpath("//b[.='Account Created!']")).isDisplayed());//passed
+
 //        15. Click 'Continue' button
+
+        driver.findElement(By.linkText("Continue")).click();
+
 //        16. Verify that 'Logged in as username' is visible
+
+        Assert.assertTrue(driver.findElement(By.xpath("(//b)[1]")).isDisplayed());//passed
+
 //        17. Click 'Delete Account' button
+
+        driver.findElement(By.xpath("//a[.=' Delete Account']")).click();
+
 //        18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+
+        Assert.assertTrue(driver.findElement(By.xpath("//b[.='Account Deleted!']")).isDisplayed());//passed
+
+
     }
 }
